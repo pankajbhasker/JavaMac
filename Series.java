2080b8ecd965167f4d9e66f2ae008b95156f8530
Pankaj K Bhasker <pankaj.bhasker709@gmail.com>
2022-01-25 11:26:41 +0000

    Add files via upload
    Adding series prog

---
+++ Series.java
@@ -0,0 +1,40 @@
+public class Series {
+
+  public static void main (String args[])  {
+
+    for(int i =7 ; i > 0; i--){
+      for(int j =i ; j > 0; j --) {
+        System.out.println();
+        System.out.println(j);
+      }
+    }
+
+  }
+
+  //
+  public void showFibonacciSeries(int tillNum){
+    int a = 1;
+    int  b = 1; //  112358
+    System.out.print(a);
+
+
+
+  }
+
+  private int getNextElement(int num){
+   int  nextVal = 0;
+/*
+    public static Map<String, List<String>>  findPairs(String[][] studentCourse) {
+      Map<String, List<String>> map = new HashMap<>(); // {[student id pair], [courses]}
+      Set<Integer> studentIds = new HashSet<>();
+    }
+
+    public static Map<String, List<String>> findSharedCourses(String studentPair, List<String> studentCoursePairs) {
+
+    }
+    */
+ 
+  }
+
+
+}
